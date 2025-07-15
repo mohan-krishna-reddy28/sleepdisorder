fa2cfe9699cd454a063b3bb111ad0bd90f5c125b
Mohan <mohankrishna28803@gmail.com>
2025-07-15 21:09:26 +0530

    commit

--- self/settings.py
+++ self/settings.py
@@ -53,14 +53,14 @@ ROOT_URLCONF = 'self.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')], # This looks fine
+        'DIRS': [], # Or your template directories
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.messages',
+                'django.contrib.messages.context_processors.messages', # <--- ENSURE THIS LINE IS HERE
             ],
         },
     },
